0309e473d33df048864c46b31f88d687844a0a39
Fiona Waters <fiwaters6@gmail.com>
2023-03-12 20:52:19 +0000

    checking for username uniqueness

--- controllers/user.go
+++ controllers/user.go
@@ -22,7 +22,7 @@ func GetUsers(c *gin.Context) {
 	//create an array the same length as the number of results
 	data := make([]models.User, len(results))
 
-	//loop over the results and individually marshal into Listing struct
+	//loop over the results and individually marshal into user struct
 	for i, r := range results {
 		var u models.User
 
@@ -34,7 +34,7 @@ func GetUsers(c *gin.Context) {
 		data[i] = u
 	}
 
-	log.Default().Println("data getUsersssss = ", data)
+	log.Default().Println("data getUsers = ", data)
 
 	c.IndentedJSON(http.StatusOK, data)
 }
@@ -73,62 +73,67 @@ func AddUserDetails(c *gin.Context) {
 
 	var id = newUser.ID
 	newUser.ID = ""
-	if newUser.AccountStatus == "Archived" {
-		newUser.ArchiveTimestamp = time.Now()
-		listingsRef := client.NewRef("listings")
-		results, err := listingsRef.OrderByKey().GetOrdered(ctx)
-		if err != nil {
-			log.Fatalln("Error querying database:", err)
-		}
-		for _, v := range results {
-			var l models.Listing
-			if err := v.Unmarshal(&l); err != nil {
-				log.Fatalln("Error unmarshaling result", err)
+	uniqueUsername := isUsernameUnique(newUser.UserName)
+	log.Printf("uniqueusername %v", uniqueUsername)
+	if uniqueUsername {
+		if newUser.AccountStatus == "Archived" {
+			newUser.ArchiveTimestamp = time.Now()
+			listingsRef := client.NewRef("listings")
+			results, err := listingsRef.OrderByKey().GetOrdered(ctx)
+			if err != nil {
+				log.Fatalln("Error querying database:", err)
 			}
-			if l.UserId == id {
-				l.Status = "Archived"
+			for _, v := range results {
+				var l models.Listing
+				if err := v.Unmarshal(&l); err != nil {
+					log.Fatalln("Error unmarshaling result", err)
+				}
+				if l.UserId == id {
+					l.Status = "Archived"
 
-				err = listingsRef.Update(ctx, map[string]interface{}{v.Key(): l})
-				{
+					err = listingsRef.Update(ctx, map[string]interface{}{v.Key(): l})
+					{
+						if err != nil {
+							log.Printf("error updating listings %v", err)
+						}
+					}
+				}
+			}
+		}
+		if newUser.AccountStatus == "Active" {
+			newUser.Role = "user"
+			// TODO below - only add creation timestamp if this is the first time the user has become active(not after restoring account)
+			//	newUser.CreationTimestamp = time.Now()
+			listingsRef := client.NewRef("listings")
+			results, err := listingsRef.OrderByKey().GetOrdered(ctx)
+			if err != nil {
+				log.Fatalln("Error querying database:", err)
+			}
+
+			for _, v := range results {
+				var l models.Listing
+				if err := v.Unmarshal(&l); err != nil {
+					log.Fatalln("Error unmarshaling result", err)
+				}
+
+				if l.UserId == id {
+					l.Status = "Available"
+					err = listingsRef.Update(ctx, map[string]interface{}{v.Key(): l})
 					if err != nil {
 						log.Printf("error updating listings %v", err)
 					}
 				}
 			}
 		}
-	}
-	if newUser.AccountStatus == "Active" {
-		newUser.Role = "user"
-		// TODO below - only add creation timestamp if this is the first time the user has become active(not after restoring account)
-		//	newUser.CreationTimestamp = time.Now()
-		listingsRef := client.NewRef("listings")
-		results, err := listingsRef.OrderByKey().GetOrdered(ctx)
+		//log.Printf("timestamp difference %v", newUser.ArchiveTimestamp.Sub(time.Now()))
+		err := ref.Update(ctx, map[string]interface{}{id: newUser})
 		if err != nil {
-			log.Fatalln("Error querying database:", err)
+			log.Fatalln("Error setting value:", err)
 		}
-
-		for _, v := range results {
-			var l models.Listing
-			if err := v.Unmarshal(&l); err != nil {
-				log.Fatalln("Error unmarshaling result", err)
-			}
-
-			if l.UserId == id {
-				l.Status = "Available"
-				err = listingsRef.Update(ctx, map[string]interface{}{v.Key(): l})
-				if err != nil {
-					log.Printf("error updating listings %v", err)
-				}
-			}
-		}
+		c.IndentedJSON(http.StatusCreated, newUser)
+	} else {
+		c.AbortWithStatus(400)
 	}
-	log.Printf("timestamp difference %v", newUser.ArchiveTimestamp.Sub(time.Now()))
-	err := ref.Update(ctx, map[string]interface{}{id: newUser})
-	if err != nil {
-		log.Fatalln("Error setting value:", err)
-	}
-
-	c.IndentedJSON(http.StatusCreated, newUser)
 }
 
 //TODO create a cron job that utilises DeleteUser function
@@ -177,4 +182,33 @@ func DeleteUser(userId string) error {
 	log.Printf("successfully deleted user: %s ", userId)
 
 	return nil
+}
+
+func isUsernameUnique(userName string) bool {
+	//get all user profiles
+	log.Println("hello line 189")
+	ctx, client, _ := InitialiseFirebaseApp()
+
+	//Create Ref for users
+	ref := client.NewRef("users")
+	//retrieve the users in order of the keys
+	results, err := ref.OrderByKey().GetOrdered(ctx)
+	if err != nil {
+		log.Fatalln("Error querying database:", err)
+	}
+	// loop through them and check the username against the profile usernames
+	var res = true
+	for _, r := range results {
+		var u models.User
+		if err := r.Unmarshal(&u); err != nil {
+			log.Fatalln("Error unmarshaling result:", err)
+		}
+		u.ID = r.Key()
+		// if suggested username matches existing username return false
+		if u.UserName == userName {
+			res = false
+			break
+		}
+	}
+	return res
 }
